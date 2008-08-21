202bcaf6f35b4bf7a598b31a2f1b62b8a79729cc
Nathan Kinkade <nkinkade@npk.(none)>
2008-08-21 13:03:07 -0700

    At the moment support/jurisdictions?locale=hr seems to miserably fail.  Added it as a test

--- cc_api/tests/functional/test_support.py
+++ cc_api/tests/functional/test_support.py
@@ -7,6 +7,12 @@ class TestSupportController(TestController):
         response = self.app.get('/support/jurisdictions')
         self.assertEqual(response.status, 200)
 
+    # Make sure that passing the ?locale= arg works
+    def test_jurisdictions(self):
+
+        response = self.app.get('/support/jurisdictions?locale=hr')
+        self.assertEqual(response.status, 200)
+
     def test_jurisdictions_js(self):
 
         response = self.app.get('/support/jurisdictions.js')
